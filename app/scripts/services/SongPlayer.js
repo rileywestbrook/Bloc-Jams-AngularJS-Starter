2b89e38958408e355903638d25a88e48fd8c2243
Riley Westbrook <westbrookriley@gmail.com>
2017-10-05 19:22:51 -0400

    assignment 8 complete

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -21,8 +21,7 @@
     */
     var setSong = function(song) {
       if (currentBuzzObject) {
-        currentBuzzObject.stop();
-        SongPlayer.currentSong.playing = null;
+        stopSong(SongPlayer.currentSong);
       }
 
       currentBuzzObject = new buzz.sound(song.audioUrl, {
@@ -41,6 +40,16 @@
     var playSong = function (song) {
       currentBuzzObject.play();
       song.playing = true;
+    }
+
+    /**
+    * @function stopSong
+    * @desc Stops current song and sets song.playing to null
+    * @param {Object} song
+    */
+    var stopSong = function(song) {
+      currentBuzzObject.stop();
+      song.playing = null;
     }
 
     /**
@@ -96,6 +105,25 @@
       currentSongIndex--;
 
       if (currentSongIndex < 0) {
+        currentBuzzObject.stop();
+        SongPlayer.currentSong.playing = null;
+      } else {
+        var song = currentAlbum.songs[currentSongIndex];
+        setSong(song);
+        playSong(song);
+      }
+    }
+
+    /**
+    * @function SongPlayer.next
+    * @desc sends index to next song
+    * @param {Number} song
+    */
+    SongPlayer.next = function() {
+      var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+      currentSongIndex++;
+
+      if (currentSongIndex > currentAlbum.songs.length) {
         currentBuzzObject.stop();
         SongPlayer.currentSong.playing = null;
       } else {
